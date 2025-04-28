8cbe68f392c7ec54a97d2399277cb025dc5533fd
Arran <arranchilles@gmail.com>
2025-04-28 22:03:07 +0100

    fixed camera, now smooth full int in correct direction. Mario velocity still busted

--- js/classes/Camera.js
+++ js/classes/Camera.js
@@ -6,23 +6,23 @@ export default class Camera{
     }
     moveRight(xdiff){
         this.posX += xdiff;
-        xdiff = Math.floor(xdiff);
         //this.ctx.save();
-        this.ctx.translate(-xdiff, 0);
+        this.ctx.setTransform(1, 0, 0, 1, 0, 0); 
+        this.ctx.translate(Math.round(-this.posX), 0);
         //this.ctx.restore();
     }
     moveLeft(xdiff){
         this.posX -= xdiff;
-        xdiff = Math.floor(xdiff);
-       // this.ctx.save();
-        this.ctx.translate(xdiff, 0);
+        //this.ctx.save();
+        this.ctx.setTransform(1, 0, 0, 1, 0, 0); 
+        this.ctx.translate(Math.round(this.posX), 0);
         //this.ctx.restore();
     }
     move(xdiff){ // when derrived from velocity,
         this.posX += xdiff;
         //this.ctx.save();
-        xdiff = Math.floor(xdiff);
-        this.ctx.translate(-xdiff, 0);
+        this.ctx.setTransform(1, 0, 0, 1, 0, 0); 
+        this.ctx.translate(Math.round(-this.posX), 0);
         //this.ctx.restore();
     }
     panTo(endX, endY, time){
